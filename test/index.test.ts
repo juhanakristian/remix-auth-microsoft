25fe5640de8f02a3dab27924e62f63cda6405493
Benjamin Indermühle <benjamin@diginlab.com>
2023-11-03 18:08:13 +0100

    rolled back change to the test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { createCookieSessionStorage } from "@remix-run/node";
+import { createCookieSessionStorage } from "@remix-run/server-runtime";
 import { MicrosoftStrategy } from "../src";
 
 describe(MicrosoftStrategy, () => {
